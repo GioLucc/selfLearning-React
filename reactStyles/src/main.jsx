294d5d21de7ec3e1a80ff91539015cdc66cc6f36
GioLucc <giolucarna@gmail.com>
2023-12-24 04:52:15 -0300

    Feat :sparkles: - Added Styles!

---
+++ reactStyles/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css'
+import * as components from './variables'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <components.Variables/>
+  </React.StrictMode>,
+)
