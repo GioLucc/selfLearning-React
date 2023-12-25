1f5668b730c2d503d135767dbaecf8c9b93372a4
GioLucc <giolucarna@gmail.com>
2023-12-25 05:06:48 -0300

    Feat :sparkles: - Implemented ternaries with hooks and map!

---
+++ ternaries_and_conditionals/src/ListingApp.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Items = ({ content, seen }) => {
+    return (
+        <li>
+            {content}
+            {seen ? '✅' : '❌'}
+            {/* {visto && '✅'} only if*/}
+        </li>
+    )
+}
+
+export const ListingApp = () => {
+    const addTasks = () =>
+    {
+        // Content is an array, adding a new content means using [with the format below]
+        setContent([...content, {content: 'New content', seen: false}])
+    }
+    let contentList = [
+        {content: "Components", seen: true},
+        {content: "Variables", seen: true},
+        {content: "Fragment", seen: false},
+        {content: "Props", seen: true},
+        {content: "Events", seen: true}
+    ]
+
+    // usss Use state snippet
+    const [content, setContent] = useState(contentList)
+    return (
+        <>
+            <h1>List of this course content</h1>
+            <ol>
+                                        {/* Key unica necesaria devuelve un item con sus propiedades de item que obtengo desde mi content list mediante el hook con useState*/}  
+                {content.map(item => <Items key={item.content} content={item.content} seen={item.seen}></Items>)}
+                <button onClick={() => addTasks()}>Add a new content</button>
+            </ol>
+        </>
+    )
+}
---
+++ ternaries_and_conditionals/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ListingApp } from './ListingApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ListingApp/>
+  </React.StrictMode>,
+)
