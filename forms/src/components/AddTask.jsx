19bf318f5d143d78e0cbe2b548145d046e138d41
GioLucc <giolucarna@gmail.com>
2024-01-03 12:02:30 -0300

    Feat :zap: - Improved communication now child emits and father handles it!

--- forms/src/components/AddTask.jsx
+++ forms/src/components/AddTask.jsx
@@ -6,13 +6,9 @@ export const AddTask = ({ addNewTask }) => { // AddTask now receives a property 
     const onInputChange = (event) => {
         setInputValue(event.target.value)
     }
-    const onSubmit = (event) => {
-        const parsedValue = { // since we can't send a string we turn it into a object
-            content: inputValue,
-            seen: false
-        }
+    const onSubmit = (event) => { // this child now emits the string
         event.preventDefault()
-        addNewTask(task => [...task, parsedValue])  // and we send the value back to the parent with a new array value with the ... setContent formatting
+        addNewTask(inputValue)  
     }
 
     return (
--- forms/src/components/ListingApp.jsx
+++ forms/src/components/ListingApp.jsx
@@ -19,22 +19,34 @@ export const ListingApp = () => {
     //     setContent([...content, {content: 'New content', seen: false}])
     // }
     let contentList = [
-        { content: "Components", seen: true },
-        { content: "Variables", seen: true },
-        { content: "Fragment", seen: false },
-        { content: "Props", seen: true },
-        { content: "Events", seen: true }
+        { id: 1, content: "Components", seen: true },
+        { id: 2, content: "Variables", seen: true },
+        { id: 3, content: "Fragment", seen: false },
+        { id: 4, content: "Props", seen: true },
+        { id: 5, content: "Events", seen: true }
     ]
 
     // usss Use state snippet
-    const [content, setContent] = useState(contentList)
+    const [content, setContent] = useState(contentList) // Hook to add into the content list
+
+    const onTaskAdd = (value) => {
+        let trimmedValue = value.trim
+        if (trimmedValue < 1) return
+        const parsedValue = {
+            id: content.length+1,
+            content: trimmedValue,
+            seen: false
+        }
+        setContent([...content, parsedValue])
+    }
+
     return (
         <>
             <h1>List of this course content</h1>
             <ol>
                 {/* Key unica necesaria devuelve un item con sus propiedades de item que obtengo desde mi content list mediante el hook con useState*/}
-                {content.map(item => <Items key={item.content} content={item.content} seen={item.seen}></Items>)}
-                 <AddTask addNewTask = {setContent}></AddTask> {/*property with hook for the child to resend us new content */}
+                {content.map(item => <Items key={item.id} content={item.content} seen={item.seen}></Items>)}
+                <AddTask addNewTask={onTaskAdd}></AddTask> {/*property with hook for the child to resend us new content */}
                 {/* <button onClick={() => addTasks()}>Add a new content</button> */}
             </ol>
         </>
